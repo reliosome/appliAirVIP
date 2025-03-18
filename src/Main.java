10e1e7ef0b2ef77104115f7c4f63db35c3dc1672
reliosome <aurele.a.collin@gmail.com>
2025-03-18 10:17:05 -0400

    Fenetre de depart

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        JFrame frame = new JFrame();
+        frame.setTitle("AirVIP Gestionnaire");
+        frame.setLayout(new BorderLayout());
+
+        JPanel mainPanel = new JPanel();
+        frame.add(mainPanel, BorderLayout.CENTER);
+
+        frame.setSize(new Dimension(1280,720));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setLocationRelativeTo(null);
+
+        frame.setResizable(true);    // à voir
+
+        frame.setVisible(true);
+
     }
 }
