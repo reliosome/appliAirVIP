adc6dd4abd4683426136e5bf0bd3ee17d5872e5d
rdbz23 <rayandbz9@gmail.com>
2025-03-19 16:16:44 -0400

    ajout de la page GestionnaireVolsdisponibles.java

---
+++ src/pages/GestionnaireVolsdisponibles.java
@@ -0,0 +1,19 @@
+package pages;
+
+public class GestionnaireVolsdisponibles {
+
+
+    //Accès a la liste des vols indispo et dispo
+
+
+
+    // RENDRE VOL DIPSONIBLE/INDISPONIBLE  Masquer ou afficher pour le client
+
+    //Créer vols / annuler vols
+
+
+
+
+
+
+}
