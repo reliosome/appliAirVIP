789f757fc0d83d5351de80f388702b3f0c89163f
rdbz23 <rayandbz9@gmail.com>
2025-03-19 16:38:17 -0400

    Add files via upload

---
+++ src/pages/GestionnaireVolsdisponibles.java
@@ -0,0 +1,25 @@
+package pages;
+
+public class GestionnaireVolsdisponibles {
+
+
+    //Accès a la liste des vols indispo et dispo
+
+
+
+    // RENDRE VOL DIPSONIBLE/INDISPONIBLE  Masquer ou afficher pour le client
+
+    //Créer vols / annuler vols
+
+
+    // verifier connexion
+
+    public boolean VerifierConnexion () {
+
+
+    }
+
+
+
+
+}
